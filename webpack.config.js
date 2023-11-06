78ca9fde03fd3ba98aa9d3ac11e50493754a9ae3
Seva <seva.leonov@hotmail.com>
2023-11-06 22:52:42 +0700

    add webpack configuration to include styles.css and to enabling source maps

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,32 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+
   entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+  
+  devtool: 'inline-source-map',
+
   plugins: [
     new HtmlWebpackPlugin({
+        template: './src/index.html',
         filename: 'index.html',
         inject: 'head',
         scriptLoading: 'defer',
     })
-  ]
+  ],
+
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
+  module: {
+    rules: [
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+    ],
+  },
 };
