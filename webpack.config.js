05b72f71ce1d11897ce58860106591bcf8d4b073
Seva <seva.leonov@hotmail.com>
2023-11-06 23:29:26 +0700

    configure webpack to do watch and configure webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-
+  mode: 'development',
   entry: './src/index.js',
-  
+
   devtool: 'inline-source-map',
+
+  devServer: {
+    static: './src',
+  },
 
   plugins: [
     new HtmlWebpackPlugin({
