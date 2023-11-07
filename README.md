9bbfb2895550d8749c0ae1d697c7c890562e6f3e
Seva <seva.leonov@hotmail.com>
2023-11-07 15:51:07 +0700

    create DOM via js

--- README.md
+++ README.md
@@ -3,3 +3,12 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin git@github.com:sevleo/project_name.git
 git push -u origin main
+
+
+Background image credit:
+Rizky Subagja
+https://unsplash.com/photos/two-clear-drinking-glasses-on-top-of-brown-wooden-table-1k7TnX5GAww
+
+Menu animation inspired by:
+Hyperplexed 
+https://www.youtube.com/watch?v=NUeCNvYY_x4
--- src/index.js
+++ src/index.js
@@ -1,6 +1,54 @@
 import './styles.css';
 
-const menu = document.querySelector('.menu');
+const body = document.querySelector('body');
+
+const main = document.createElement('div');
+main.id = 'main';
+body.append(main);
+
+const menu = document.createElement('div');
+menu.classList.add('menu');
+main.append(menu);
+
+const content = document.createElement('div');
+content.classList.add('content');
+main.append(content);
+
+const backgroundImage = document.createElement('div');
+backgroundImage.classList.add('background-image');
+main.append(backgroundImage);
+
+const menuItems = document.createElement('div');
+menuItems.classList.add('menu-items');
+menu.append(menuItems);
+
+const menuBackgroundPattern = document.createElement('div');
+menuBackgroundPattern.classList.add('menu-background-pattern');
+menu.append(menuBackgroundPattern);
+
+const menuItem1 = document.createElement('a');
+menuItem1.setAttribute('href', '/');
+menuItem1.classList.add('menu-item');
+menuItem1.textContent = 'Home';
+menuItems.append(menuItem1);
+
+const menuItem2 = document.createElement('a');
+menuItem2.setAttribute('href', '/');
+menuItem2.classList.add('menu-item');
+menuItem2.textContent = 'Shop';
+menuItems.append(menuItem2);
+
+const menuItem3 = document.createElement('a');
+menuItem3.setAttribute('href', '/');
+menuItem3.classList.add('menu-item');
+menuItem3.textContent = 'About';
+menuItems.append(menuItem3);
+
+const menuItem4 = document.createElement('a');
+menuItem4.setAttribute('href', '/');
+menuItem4.classList.add('menu-item');
+menuItem4.textContent = 'Contact Us';
+menuItems.append(menuItem4);
 
 Array.from(document.getElementsByClassName('menu-item')).forEach((item, index) => {
     item.onmouseover = () => {
